de46ba5b938c31995b509a085af28270a1970f82
Thingholm <rasmusthingholm@gmail.com>
2025-08-20 19:10:45 +0200

    Added supabase client

---
+++ www/src/db/riderPoints.ts
@@ -0,0 +1,31 @@
+import { supabase } from "@/utils/supabase/client";
+import { QueryData } from "@supabase/supabase-js";
+import { unstable_cache } from "next/cache";
+
+export const getAllRiderPointsWithNationAndTeam = unstable_cache(async () => {
+    const { data, error } = await riderPointsWithNationAndTeamQuery;
+    
+    if (error) { throw error; }
+
+    return data as RiderPointsWithNation;
+
+}, ["allRiderPointsWithNation"], { revalidate: 60 * 60 });
+
+const riderPointsWithNationAndTeamQuery = supabase
+        .from("rider_points")
+        .select(`
+            *,
+            riders (
+                *,
+                nations (
+                    *
+                ),
+                teams (
+                    *
+                )
+            )
+        `
+        )
+        .order("points", { ascending: false });
+
+export type RiderPointsWithNation = QueryData<typeof riderPointsWithNationAndTeamQuery>
--- www/src/utils/supabase/server.ts
+++ www/src/utils/supabase/server.ts
@@ -2,7 +2,6 @@ import { createServerClient } from "@supabase/ssr";
 import { Database } from "./database.types";
 
 export async function createClient() {
-
     return createServerClient<Database>(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_KEY!,
