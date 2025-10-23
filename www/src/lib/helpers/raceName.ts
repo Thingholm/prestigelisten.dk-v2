ca73a52c55fd1876c04592a97d3c837413195bfa
Thingholm <rasmusthingholm@gmail.com>
2025-10-23 12:48:48 +0200

    Fixed issue with championship names not translating

--- www/src/lib/helpers/raceName.ts
+++ www/src/lib/helpers/raceName.ts
@@ -17,16 +17,22 @@ export function getResultRaceName(race: MetaRace, t: ReturnType<typeof useTransl
         return `${t(`nations.${race.nations.code}.adjective`)} ${t("champion")}`;
     }
 
+    const raceIdsForTranlation = [225, 226, 227, 228, 229, 230, 231, 289];
+
+    if (raceIdsForTranlation.includes(race.id)) {
+        return t((`metaRaces.${race.id}`))
+    }
+
     return race.name
 }
 
 export function getRaceName(race: MetaRace, t: ReturnType<typeof useTranslations>) {
     if (race.name.includes("mester i enkeltstart") && race.nations) {
-        return `${t(`nations.${race.nations.code}.adjective`)}${t("championshipITT")}`;
+        return `${t(`nations.${race.nations.code}.adjective`)} ${t("championshipITT")}`;
     }
 
     if (race.name.includes("mester") && race.nations) {
-        return `${t(`nations.${race.nations.code}.adjective`)}${t("championship")}`;
+        return `${t(`nations.${race.nations.code}.adjective`)} ${t("championship")}`;
     }
     
     const raceIdsForTranlation = [225, 226, 227, 228, 229, 230, 231, 289];
