a8515aa1e488c761695a46c18748ddc545a8c96f
Thingholm <rasmusthingholm@gmail.com>
2025-10-30 21:13:01 +0100

    Installed and set up ef core

---
+++ backend/src/Prestigelisten.Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Prestigelisten.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<DbContext> options)
+        : base(options)
+    { }
+}
