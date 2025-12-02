4635f28ae4ec95ad315b2e841acf014c2a8e9c3c
Thingholm <rasmusthingholm@gmail.com>
2025-12-02 17:52:25 +0100

    Changed order of items in footer nav

--- www/src/components/layout/footer/Footer.tsx
+++ www/src/components/layout/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { getTranslations } from "next-intl/server"
+import { getLocale, getTranslations } from "next-intl/server"
 import Image from "next/image";
 import logo from "@/../public/logo.jpg"
 import Link from "next/link";
@@ -8,6 +8,7 @@ import { urls } from "@/lib/constants/urls";
 
 export default async function Footer() {
     const t = await getTranslations("navigation");
+    const locale = await getLocale();
 
     return (
         <footer className="bg-secondary-950 text-white py-8 px-3 sm:px-16 w-full">
@@ -25,24 +26,23 @@ export default async function Footer() {
                     <ul>
                         <li><Link className="hover:underline hover:text-primary-500" href={getRidersListUrl()}>{t("ridersAlltime")}</Link></li>
                         <li><Link className="hover:underline hover:text-primary-500" href={getRidersListUrl({ status: "active" })}>{t("active")}</Link></li>
-                        <li><Link className="hover:underline hover:text-primary-500" href={getRidersListUrl({ nations: [14] })}>{t("danish")}</Link></li>
-                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listNations"]}>{t("nations")}</Link></li>
-                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listRiders3YearPeriod"]}>{t("3YearPeriod")}</Link></li>
-                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listRidersGreatestSeasons"]}>{t("greatestSeasons")}</Link></li>
-                        <li><Link className="hover:underline hover:text-primary-500" href={urls["year"]}>{t("mostPointsEachYear")}</Link></li>
-                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listRidersAges"]}>{t("greatestByAge")}</Link></li>
                         <li><Link className="hover:underline hover:text-primary-500" href={getRidersListUrl({ isSingleYear: true })}>{t("greatestByYear")}</Link></li>
+                        {locale == "da" && <li><Link className="hover:underline hover:text-primary-500" href={getRidersListUrl({ nations: [14] })}>{t("danish")}</Link></li>}
+                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listNations"]}>{t("nations")}</Link></li>
+                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listRidersGreatestSeasons"]}>{t("greatestSeasons")}</Link></li>
+                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listRidersAges"]}>{t("greatestByAge")}</Link></li>
+                        <li><Link className="hover:underline hover:text-primary-500" href={urls["listRiders3YearPeriod"]}>{t("3YearPeriod")}</Link></li>
                     </ul>
                 </div>
                 <div>
                     <h4 className="font-bold opacity-50 uppercase mb-2">{t("more")}</h4>
                     <ul>
-                        <li><Link className="hover:underline hover:text-primary-500" href={urls["about"]}>{t("about")}</Link></li>
                         <li><Link className="hover:underline hover:text-primary-500" href={urls["pointSystem"]}>{t("pointSystem")}</Link></li>
+                        <li><Link className="hover:underline hover:text-primary-500" href={urls["year"]}>{t("seasonOverview")}</Link></li>
                         <li><Link className="hover:underline hover:text-primary-500" href={urls["team"]}>{t("teams")}</Link></li>
                         <li><Link className="hover:underline hover:text-primary-500" href={urls["compare"]}>{t("compare")}</Link></li>
                         <li><Link className="hover:underline hover:text-primary-500" href={urls["calendar"]}>{t("calendar")}</Link></li>
-                        <li><Link className="hover:underline hover:text-primary-500" href={urls["rawData"]}>{t("rawData")}</Link></li>
+                        <li><Link className="hover:underline hover:text-primary-500" href={urls["about"]}>{t("about")}</Link></li>
                         <li><Link className="hover:underline hover:text-primary-500" href={urls["quiz"]}>{t("quiz")}</Link></li>                        
                     </ul>
                 </div>
