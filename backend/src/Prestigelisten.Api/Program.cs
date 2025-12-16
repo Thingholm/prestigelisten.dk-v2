5ea09d243f990babbc6881f6b7fdbd4846d3c3c1
Thingholm <rasmusthingholm@gmail.com>
2025-12-16 10:10:43 +0100

    Removed test code

--- backend/src/Prestigelisten.Api/Program.cs
+++ backend/src/Prestigelisten.Api/Program.cs
@@ -56,11 +56,4 @@ api.MapGet("/health", () => Results.Ok("Healthy"));
 
 api.MapResultEndpoints();
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.SetCommandTimeout(TimeSpan.FromMinutes(10));
-    dbContext.Database.Migrate();
-}
-
 app.Run();
