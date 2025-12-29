5ce8ccd07bf9216eb5e8305ebb1410396986067a
Thingholm <rasmusthingholm@gmail.com>
2025-12-29 15:39:22 +0100

    Added metadata to season overview redirect page

--- www/src/app/[lang]/year/page.tsx
+++ www/src/app/[lang]/year/page.tsx
@@ -1,5 +1,16 @@
 import { getYearUrl } from "@/lib/helpers/urls";
+import { getTranslations } from "next-intl/server";
 import { redirect } from "next/navigation";
+
+export async function generateMetadata({ params }: { params: Promise<{ locale: "en" | "da" }> }) {
+    const { locale } = await params;
+    const t = await getTranslations({locale, namespace: 'metadata.seasonRedirectPage'});
+    
+    return {
+        title: t('title'),
+        description: t("description")
+    };
+}
 
 export default function Page() {
     redirect(`${getYearUrl()}/${new Date().getFullYear()}`)
