11f09afdbc98821edf4117adadbe67a1ca88a671
Thingholm <rasmusthingholm@gmail.com>
2026-01-08 14:38:43 +0100

    Fixed locale import error

--- www/src/app/[lang]/rider/[id]/[[...name]]/page.tsx
+++ www/src/app/[lang]/rider/[id]/[[...name]]/page.tsx
@@ -13,9 +13,9 @@ import { getRidersPreviousNationalities } from "@/db/prevNationalities";
 import { getTranslations } from "next-intl/server";
 import { deserializeQueryString } from "@/lib/helpers/urls";
 
-export async function generateMetadata({ params }: { params: Promise<{ locale: "en" | "da", id: number,  name: string[] }> }) {
-    const { locale, id, name } = await params;
-    const t = await getTranslations({locale, namespace: 'metadata.riderPage'});
+export async function generateMetadata({ params }: { params: Promise<{ lang: "en" | "da", id: number,  name: string[] }> }) {
+    const { lang, id, name } = await params;
+    const t = await getTranslations('metadata.riderPage');
     const riderName = name?.[0] ? deserializeQueryString(name[0]) : t("rider")
     
     return {
@@ -25,10 +25,10 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: "
         creator: "@prestigelisten",
         card: "summary_large_image",
         openGraph: {
-            images: [`https://ijyqomzpcigbnwjjohrd.supabase.co/storage/v1/object/public/${locale}_rider-images/${id}.png`]
+            images: [`https://ijyqomzpcigbnwjjohrd.supabase.co/storage/v1/object/public/${lang}_rider-images/${id}.png`]
         },
         twitter: {
-            images: [`https://ijyqomzpcigbnwjjohrd.supabase.co/storage/v1/object/public/${locale}_rider-images/${id}.png`]
+            images: [`https://ijyqomzpcigbnwjjohrd.supabase.co/storage/v1/object/public/${lang}_rider-images/${id}.png`]
         }
     };
 }
