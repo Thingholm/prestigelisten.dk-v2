87d9de5b15ba53777a718141d2e0c1b92fbb5b2d
Thingholm <rasmusthingholm@gmail.com>
2026-01-20 19:48:08 +0100

    Outcommented functions not ready for production

--- www.admin/app/dashboard/layout.tsx
+++ www.admin/app/dashboard/layout.tsx
@@ -1,13 +1,21 @@
 import { DashboardSidebar } from "@/components/dashboard-sidebar"
+import { LogoutButton } from "@/components/logout-button"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import Link from "next/link"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <DashboardSidebar />
       <main className="flex flex-1 flex-col gap-4">
-        <header className="bg-background sticky top-0 flex h-12 shrink-0 items-center gap-2 border-b px-4">
-            <SidebarTrigger />
+        <header className="bg-background sticky top-0 flex h-12 shrink-0 justify-between items-center border-b px-4">
+            <div>
+              <SidebarTrigger />
+            </div>
+            <div className="flex items-center gap-2">
+              <Link href="/auth/update-password" className="text-sm hover:underline">Ændre kodeord</Link>
+              <LogoutButton />
+            </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
             {children}
--- www.admin/components/dashboard-sidebar.tsx
+++ www.admin/components/dashboard-sidebar.tsx
@@ -23,11 +23,11 @@ const items = [
         url: "/dashboard/sync-data",
         icon: ArrowDownToLine,
     },
-    {
-        title: "X/Twitter billeder",
-        url: "/dashboard/twitter-images",
-        icon: Wallpaper,
-    },
+    // {
+    //     title: "X/Twitter billeder",
+    //     url: "/dashboard/twitter-images",
+    //     icon: Wallpaper,
+    // },
 ]
 
 export function DashboardSidebar() {
